40a87176f978ac7f5018b35b7152e75282871989
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-15 16:14:18 +0100

    Fix: Changed site config

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+const siteConfig = {
+  domain: "https://getmedsync.vercel.app",
+  social: {},
+  name: "Medsync",
+  headline: "Medsync",
+  description: "Connecting you to better health",
+  keywords: "healthcare, medsync, hospital, hms, hospital management system",
+  image: "https://medsync.vercel.app/images/banner.png",
+};
+
+export default siteConfig;
