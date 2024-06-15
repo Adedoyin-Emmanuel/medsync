9b11dfe71bccd95a9808b8c5652443cbba2aa5d9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-15 15:00:41 +0100

    Feat: Project restart

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -15,5 +15,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
 };
+export default config;
